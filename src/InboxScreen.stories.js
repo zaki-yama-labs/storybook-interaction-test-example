9e0f078939bd9d30ada20957aec175319c675088
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-04-15 00:57:51 +0900

    npx degit chromaui/ui-testing-guide-code#dc9bacae842f5250aad544b139dc9d63a48bbd1e taskbox

---
+++ src/InboxScreen.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { rest } from 'msw';
+import { within, findByRole, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { InboxScreen } from './InboxScreen';
+import { Default as TaskListDefault } from './components/TaskList.stories';
+
+export default {
+  component: InboxScreen,
+  title: 'InboxScreen',
+};
+
+const Template = (args) => <InboxScreen {...args} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    rest.get('/tasks', (req, res, ctx) => {
+      return res(ctx.json(TaskListDefault.args));
+    }),
+  ],
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Something',
+};
+Error.parameters = {
+  msw: [
+    rest.get('/tasks', (req, res, ctx) => {
+      return res(ctx.json([]));
+    }),
+  ],
+};
